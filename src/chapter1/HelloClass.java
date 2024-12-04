d5b94f5a6ca8fc255c718111ad48832d24a82afa
Kimeunho0710 <keh710427@gmail.com>
2024-12-05 00:14:27 +0900

    First Commit Second Commit

--- src/chapter1/HelloClass.java
+++ src/chapter1/HelloClass.java
@@ -2,6 +2,6 @@ package chapter1;
 
 public class HelloClass {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World");
     }
 }
